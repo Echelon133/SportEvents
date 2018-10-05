3928a2b478d0ef65b9014c82a74cdc792a9a3e60
Mateusz Bednarski <echelon133@gmail.com>
2018-10-05 18:01:27 +0200

    Add main spring application class

---
+++ src/main/java/ml/echelon133/sportevents/SportEventsApp.java
@@ -0,0 +1,12 @@
+package ml.echelon133.sportevents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportEventsApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SportEventsApp.class, args);
+    }
+}
