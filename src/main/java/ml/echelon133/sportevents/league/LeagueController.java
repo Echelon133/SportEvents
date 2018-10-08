502a2b558168e47c31debd69722cae83859f6240
Mateusz Bednarski <echelon133@gmail.com>
2018-10-09 00:03:46 +0200

    Update controller with getLeagues full implementation and partial implementation of getLeague

--- src/main/java/ml/echelon133/sportevents/league/LeagueController.java
+++ src/main/java/ml/echelon133/sportevents/league/LeagueController.java
@@ -1,8 +1,15 @@
 package ml.echelon133.sportevents.league;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.Resources;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 
 @RestController
 @RequestMapping("/api/leagues")
@@ -15,5 +22,19 @@ public class LeagueController {
     public LeagueController(LeagueService leagueService, LeagueResourceAssembler resourceAssembler) {
         this.leagueService = leagueService;
         this.resourceAssembler = resourceAssembler;
+    }
+
+    @GetMapping
+    public ResponseEntity<Resources<LeagueResource>> getLeagues() {
+        Resources<LeagueResource> resources = new Resources<>(resourceAssembler.toResources(leagueService.findAll()));
+        resources.add(linkTo(LeagueController.class).withRel("leagues"));
+        return new ResponseEntity<>(resources, HttpStatus.OK);
+    }
+
+    @GetMapping("/{leagueId}")
+    public ResponseEntity<LeagueResource> getLeague(@PathVariable Long leagueId) {
+        LeagueResource leagueResource = new LeagueResource(new League());
+        return new ResponseEntity<>(leagueResource, HttpStatus.OK);
+
     }
 }
