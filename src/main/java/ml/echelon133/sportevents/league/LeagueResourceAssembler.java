6a224e965164f3052eeafe7bc53217d0077812c3
Mateusz Bednarski <echelon133@gmail.com>
2018-10-09 00:42:37 +0200

    Make toResource handle getLeague exception (only because syntax requires it)

--- src/main/java/ml/echelon133/sportevents/league/LeagueResourceAssembler.java
+++ src/main/java/ml/echelon133/sportevents/league/LeagueResourceAssembler.java
@@ -1,5 +1,6 @@
 package ml.echelon133.sportevents.league;
 
+import ml.echelon133.sportevents.exception.ResourceDoesNotExistException;
 import org.springframework.hateoas.mvc.ResourceAssemblerSupport;
 import org.springframework.stereotype.Component;
 
@@ -15,8 +16,22 @@ public class LeagueResourceAssembler extends ResourceAssemblerSupport<League, Le
 
     @Override
     public LeagueResource toResource(League entity) {
-        return new LeagueResource(entity,
-                                  linkTo(LeagueController.class).withRel("leagues"),
-                                  linkTo(methodOn(LeagueController.class).getLeague(entity.getId())).withSelfRel());
+        LeagueResource resource;
+        try {
+            resource = new LeagueResource(entity,
+                    linkTo(LeagueController.class).withRel("leagues"),
+                    linkTo(methodOn(LeagueController.class).getLeague(entity.getId())).withSelfRel());
+        } catch (Exception ex) {
+            // getLeague throws Exception only if resource (League) does not exist
+            // getLeague(Long id) is used here only to generate a link to a resource (methodOn creates a proxy)
+            // see here: https://docs.spring.io/spring-hateoas/docs/current/api/org/springframework/hateoas/mvc/ControllerLinkBuilder.html#methodOn-java.lang.Class-java.lang.Object...-
+
+            // this exception handling is needed only for syntactic purposes
+            // see here: https://github.com/spring-projects/spring-hateoas/issues/82
+
+            // also see: https://github.com/search?q=This+should+never+happen&type=Code&utf8=%E2%9C%93
+            throw new RuntimeException("getLeague threw an exception - this should never happen");
+        }
+        return resource;
     }
 }
