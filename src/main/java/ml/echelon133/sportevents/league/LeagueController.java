6c2f512f6d4fd0403643ef1c0e32faf58201992d
Mateusz Bednarski <echelon133@gmail.com>
2018-10-09 19:40:49 +0200

    Update controller with working implementation of POST /api/leagues

--- src/main/java/ml/echelon133/sportevents/league/LeagueController.java
+++ src/main/java/ml/echelon133/sportevents/league/LeagueController.java
@@ -1,13 +1,14 @@
 package ml.echelon133.sportevents.league;
 
+import ml.echelon133.sportevents.exception.FailedValidationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.Resources;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 
@@ -35,5 +36,19 @@ public class LeagueController {
     public ResponseEntity<LeagueResource> getLeague(@PathVariable Long leagueId) throws Exception {
         LeagueResource leagueResource = resourceAssembler.toResource(leagueService.findById(leagueId));
         return new ResponseEntity<>(leagueResource, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<LeagueResource> createLeague(@Valid @RequestBody LeagueDto leagueDto, BindingResult result)
+            throws FailedValidationException{
+
+        if (result.hasErrors()) {
+            throw new FailedValidationException(result.getFieldErrors());
+        }
+
+        League league = leagueService.convertDtoToEntity(leagueDto);
+        League savedLeague = leagueService.save(league);
+        LeagueResource leagueResource = resourceAssembler.toResource(savedLeague);
+        return new ResponseEntity<>(leagueResource, HttpStatus.CREATED);
     }
 }
