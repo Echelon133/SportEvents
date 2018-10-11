5b37947e600c034ebdeba78c926721ebb2502493
Mateusz Bednarski <echelon133@gmail.com>
2018-10-11 12:50:09 +0200

    Add ResourceDoesNotExistException

---
+++ src/main/java/ml/echelon133/sportevents/exception/ResourceDoesNotExistException.java
@@ -0,0 +1,8 @@
+package ml.echelon133.sportevents.exception;
+
+public class ResourceDoesNotExistException extends Exception {
+
+    public ResourceDoesNotExistException(String message) {
+        super(message);
+    }
+}
