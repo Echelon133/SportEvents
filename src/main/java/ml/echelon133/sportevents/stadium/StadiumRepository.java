9356c75067f691f1fc6a8e438ff31e61d1ed1990
Mateusz Bednarski <echelon133@gmail.com>
2018-10-11 16:24:54 +0200

    Add StadiumRepository

---
+++ src/main/java/ml/echelon133/sportevents/stadium/StadiumRepository.java
@@ -0,0 +1,6 @@
+package ml.echelon133.sportevents.stadium;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StadiumRepository extends JpaRepository<Stadium, Long> {
+}
