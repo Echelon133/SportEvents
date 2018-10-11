cb7698748be97692c33c5e72c4e5ccbbd32febcc
Mateusz Bednarski <echelon133@gmail.com>
2018-10-11 18:31:08 +0200

    Update controller with implementation of GET /api/stadiums mapping

--- src/main/java/ml/echelon133/sportevents/stadium/StadiumController.java
+++ src/main/java/ml/echelon133/sportevents/stadium/StadiumController.java
@@ -1,9 +1,17 @@
 package ml.echelon133.sportevents.stadium;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.Resources;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/api/stadiums")
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+
+@RestController
+@RequestMapping("/api/stadiums")
 public class StadiumController {
 
     private StadiumService stadiumService;
@@ -13,5 +21,12 @@ public class StadiumController {
     public StadiumController(StadiumService stadiumService, StadiumResourceAssembler resourceAssembler) {
         this.stadiumService = stadiumService;
         this.resourceAssembler = resourceAssembler;
+    }
+
+    @GetMapping
+    public ResponseEntity<Resources<StadiumResource>> getStadiums() {
+        Resources<StadiumResource> resources = new Resources<>(resourceAssembler.toResources(stadiumService.findAll()));
+        resources.add(linkTo(StadiumController.class).withRel("stadiums"));
+        return new ResponseEntity<>(resources, HttpStatus.OK);
     }
 }
