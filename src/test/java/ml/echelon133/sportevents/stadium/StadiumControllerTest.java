ee9e088ceab36f3a9ab89d2b34f36b17afffee23
Mateusz Bednarski <echelon133@gmail.com>
2018-10-12 17:20:32 +0200

    Update tests with tests of PUT /api/stadiums/{id}

--- src/test/java/ml/echelon133/sportevents/stadium/StadiumControllerTest.java
+++ src/test/java/ml/echelon133/sportevents/stadium/StadiumControllerTest.java
@@ -230,4 +230,119 @@ public class StadiumControllerTest {
         assertThat(json.read("$.city").toString()).isEqualTo(stadium.getCity());
         assertThat(json.read("$.capacity").toString()).isEqualTo(stadium.getCapacity().toString());
     }
+
+    @Test
+    public void replaceStadiumStadiumDtoNullFieldsAreValidated() throws Exception {
+        StadiumDto stadiumDto = new StadiumDto(null, null, 20000);
+
+        JsonContent<StadiumDto> stadiumDtoJsonContent = jsonStadiumDto.write(stadiumDto);
+
+        // When
+        MockHttpServletResponse response = mockMvc.perform(
+                put("/api/stadiums/1")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(stadiumDtoJsonContent.getJson())
+        ).andReturn().getResponse();
+
+        // Then
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+        assertThat(response.getContentAsString()).contains("city validation error: must not be null");
+        assertThat(response.getContentAsString()).contains("name validation error: must not be null");
+    }
+
+    @Test
+    public void replaceStadiumStadiumDtoFieldLengthsAreValidated() throws Exception {
+        StadiumDto stadiumDto = new StadiumDto("n", "c", null);
+
+        JsonContent<StadiumDto> stadiumDtoJsonContent = jsonStadiumDto.write(stadiumDto);
+
+        // When
+        MockHttpServletResponse response = mockMvc.perform(
+                put("/api/stadiums/1")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(stadiumDtoJsonContent.getJson())
+        ).andReturn().getResponse();
+
+        // Then
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+        assertThat(response.getContentAsString()).contains("city validation error: length must be between 2 and 150");
+        assertThat(response.getContentAsString()).contains("name validation error: length must be between 3 and 200");
+    }
+
+    @Test
+    public void replaceStadiumStadiumDtoPositiveFieldsAreValidated() throws Exception {
+        StadiumDto stadiumDto = new StadiumDto("name", "city", -1);
+
+        JsonContent<StadiumDto> stadiumDtoJsonContent = jsonStadiumDto.write(stadiumDto);
+
+        // When
+        MockHttpServletResponse response = mockMvc.perform(
+                put("/api/stadiums/1")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(stadiumDtoJsonContent.getJson())
+        ).andReturn().getResponse();
+
+        // Then
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+        assertThat(response.getContentAsString()).contains("capacity validation error: must be greater than 0");
+    }
+
+    @Test
+    public void replaceStadiumReturnsCorrectResponseWhenResourceDoesNotExist() throws Exception {
+        StadiumDto stadiumDto = new StadiumDto("name", "city", 40000);
+
+        JsonContent<StadiumDto> stadiumDtoJsonContent = jsonStadiumDto.write(stadiumDto);
+
+        // Given
+        given(stadiumService.findById(1L)).willThrow(new ResourceDoesNotExistException("Stadium with this id does not exist"));
+
+        // When
+        MockHttpServletResponse response = mockMvc.perform(
+                put("/api/stadiums/1")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(stadiumDtoJsonContent.getJson())
+        ).andReturn().getResponse();
+
+        // Then
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.NOT_FOUND.value());
+        assertThat(response.getContentAsString()).contains("Stadium with this id does not exist");
+    }
+
+    @Test
+    public void replaceStadiumReturnsCorrectResponseOnSuccessfulReplacement() throws Exception {
+        StadiumDto stadiumDto = new StadiumDto("name", "city", 40000);
+        JsonContent<StadiumDto> stadiumDtoJsonContent = jsonStadiumDto.write(stadiumDto);
+
+        // "Saved" stadium
+        Stadium stadium = new Stadium(stadiumDto.getName(), stadiumDto.getCity(), stadiumDto.getCapacity());
+        stadium.setId(1L);
+
+        // Given
+        given(stadiumService.findById(1L)).willReturn(stadium);
+        given(stadiumService.convertDtoToEntity(any(StadiumDto.class))).willReturn(stadium);
+        given(stadiumService.save(stadium)).willReturn(stadium);
+
+        // When
+        MockHttpServletResponse response = mockMvc.perform(
+                put("/api/stadiums/1")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(stadiumDtoJsonContent.getJson())
+        ).andReturn().getResponse();
+
+        // Then
+        DocumentContext json = JsonPath.parse(response.getContentAsString());
+
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
+        assertThat(json.read("$.links[?(@.rel=='stadiums')].href").toString()).contains("/api\\/stadiums");
+        assertThat(json.read("$.links[?(@.rel=='self')].href").toString()).contains("/api\\/stadiums\\/" + stadium.getId());
+        assertThat(json.read("$.id").toString()).isEqualTo(stadium.getId().toString());
+        assertThat(json.read("$.name").toString()).isEqualTo(stadium.getName());
+        assertThat(json.read("$.city").toString()).isEqualTo(stadium.getCity());
+        assertThat(json.read("$.capacity").toString()).isEqualTo(stadium.getCapacity().toString());
+    }
 }
