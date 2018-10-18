7b0843aa271182f7fda6ec89aaed5dff35ddcdcb
Mateusz Bednarski <echelon133@gmail.com>
2018-10-18 16:40:18 +0200

    Update TeamController with the implementation of getTeams

--- src/main/java/ml/echelon133/sportevents/team/TeamController.java
+++ src/main/java/ml/echelon133/sportevents/team/TeamController.java
@@ -2,8 +2,17 @@ package ml.echelon133.sportevents.team;
 
 import ml.echelon133.sportevents.league.LeagueService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.Resources;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 
 
 @RestController
@@ -19,5 +28,22 @@ public class TeamController {
         this.teamService = teamService;
         this.leagueService = leagueService;
         this.resourceAssembler = resourceAssembler;
+    }
+
+    @GetMapping
+    public ResponseEntity<Resources<TeamResource>> getTeams(
+            @RequestParam(value = "nameContains", required = false) String name
+    ) throws Exception {
+
+        List<Team> teams;
+        if (name == null) {
+            teams = teamService.findAll();
+        } else {
+            teams = teamService.findAllByNameContaining(name);
+        }
+
+        Resources<TeamResource> resources = new Resources<>(resourceAssembler.toResources(teams));
+        resources.add(linkTo(TeamController.class).withRel("teams"));
+        return new ResponseEntity<>(resources, HttpStatus.OK);
     }
 }
