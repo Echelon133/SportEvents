b73d5d551d0b7c65902fe8ce438359470df424c8
Mateusz Bednarski <echelon133@gmail.com>
2018-10-18 18:46:18 +0200

    Update TeamController with the implementation of createTeam

--- src/main/java/ml/echelon133/sportevents/team/TeamController.java
+++ src/main/java/ml/echelon133/sportevents/team/TeamController.java
@@ -1,12 +1,16 @@
 package ml.echelon133.sportevents.team;
 
+import ml.echelon133.sportevents.exception.FailedValidationException;
+import ml.echelon133.sportevents.exception.ResourceDoesNotExistException;
 import ml.echelon133.sportevents.league.LeagueService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.Resources;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
@@ -47,5 +51,21 @@ public class TeamController {
     public ResponseEntity<TeamResource> getTeam(@PathVariable Long teamId) throws Exception {
         TeamResource teamResource = resourceAssembler.toResource(teamService.findById(teamId));
         return new ResponseEntity<>(teamResource, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<TeamResource> createTeam(@Valid @RequestBody TeamDto teamDto, BindingResult result)
+            throws FailedValidationException, ResourceDoesNotExistException {
+
+        if (result.hasErrors()) {
+            throw new FailedValidationException(result.getFieldErrors());
+        }
+
+        // Conversion fails if teamDto references a leagueId of a league that does not exist
+        Team team = teamService.convertDtoToEntity(teamDto);
+        // If conversion was successful, we can assume that 'team' object can be safely saved
+        Team savedTeam = teamService.save(team);
+        TeamResource teamResource = resourceAssembler.toResource(savedTeam);
+        return new ResponseEntity<>(teamResource, HttpStatus.CREATED);
     }
 }
