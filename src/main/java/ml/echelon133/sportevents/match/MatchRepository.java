3edcd3643114d15333c0d7bc8a50544e45c29336
Mateusz Bednarski <echelon133@gmail.com>
2018-10-24 17:22:33 +0200

    Update MatchRepository with one custom filtering by status method

--- src/main/java/ml/echelon133/sportevents/match/MatchRepository.java
+++ src/main/java/ml/echelon133/sportevents/match/MatchRepository.java
@@ -2,5 +2,8 @@ package ml.echelon133.sportevents.match;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MatchRepository extends JpaRepository<Match, Long> {
+    List<Match> findAllByStatusEquals(Match.Status status);
 }
