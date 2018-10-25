ebf312129589caa7c9067f5d6b7f963eff2fda4d
Mateusz Bednarski <echelon133@gmail.com>
2018-10-25 21:17:52 +0200

    Update MatchRepository with a custom filtering by date method

--- src/main/java/ml/echelon133/sportevents/match/MatchRepository.java
+++ src/main/java/ml/echelon133/sportevents/match/MatchRepository.java
@@ -2,8 +2,10 @@ package ml.echelon133.sportevents.match;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Date;
 import java.util.List;
 
 public interface MatchRepository extends JpaRepository<Match, Long> {
     List<Match> findAllByStatusEquals(Match.Status status);
+    List<Match> findAllByStartDateBetween(Date date1, Date date2);
 }
