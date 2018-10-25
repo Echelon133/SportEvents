db6e2eda2fa9ea65ebb18257f97ac55d4e183fe8
Mateusz Bednarski <echelon133@gmail.com>
2018-10-25 23:49:34 +0200

    Modify 'throws' clause of convertDtoToEntity to declare DateTimeParseException instead of ParseException

--- src/main/java/ml/echelon133/sportevents/match/MatchService.java
+++ src/main/java/ml/echelon133/sportevents/match/MatchService.java
@@ -2,11 +2,11 @@ package ml.echelon133.sportevents.match;
 
 import ml.echelon133.sportevents.exception.ResourceDoesNotExistException;
 
-import java.text.ParseException;
+import java.time.format.DateTimeParseException;
 import java.util.List;
 
 public interface MatchService {
-    Match convertDtoToEntity(MatchDto matchDto) throws ResourceDoesNotExistException, ParseException;
+    Match convertDtoToEntity(MatchDto matchDto) throws ResourceDoesNotExistException, DateTimeParseException;
     Match save(Match match);
     List<Match> findAll();
     List<Match> findAllWithDateWithin(String dateWithin);
