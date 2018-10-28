a0adef201c6087516a8b872aaee9b6536cfdd446
Mateusz Bednarski <echelon133@gmail.com>
2018-10-28 18:00:07 +0100

    Make MatchResourceAssembler generate self links to resources in toResource method

--- src/main/java/ml/echelon133/sportevents/match/MatchResourceAssembler.java
+++ src/main/java/ml/echelon133/sportevents/match/MatchResourceAssembler.java
@@ -1,5 +1,6 @@
 package ml.echelon133.sportevents.match;
 
+import ml.echelon133.sportevents.exception.ResourceDoesNotExistException;
 import ml.echelon133.sportevents.league.LeagueResource;
 import ml.echelon133.sportevents.league.LeagueResourceAssembler;
 import ml.echelon133.sportevents.stadium.StadiumResource;
@@ -11,6 +12,7 @@ import org.springframework.hateoas.mvc.ResourceAssemblerSupport;
 import org.springframework.stereotype.Component;
 
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
 @Component
 public class MatchResourceAssembler extends ResourceAssemblerSupport<Match, MatchResource> {
@@ -36,6 +38,7 @@ public class MatchResourceAssembler extends ResourceAssemblerSupport<Match, Matc
         TeamResource teamB = teamResourceAssembler.toResource(entity.getTeamB());
         LeagueResource league;
         StadiumResource stadium;
+        MatchResource matchResource;
 
         // entity.getLeague() can be null (league field is not required)
         try {
@@ -51,7 +54,22 @@ public class MatchResourceAssembler extends ResourceAssemblerSupport<Match, Matc
             stadium = null;
         }
 
-        return new MatchResource(entity, teamA, teamB,
-                                 league, stadium, linkTo(MatchController.class).withRel("matches"));
+        try {
+            matchResource = new MatchResource(entity, teamA, teamB, league, stadium,
+                    linkTo(MatchController.class).withRel("matches"),
+                    linkTo(methodOn(MatchController.class).getMatch(entity.getId())).withSelfRel());
+        } catch (ResourceDoesNotExistException ex) {
+            // getMatch throws Exception only if resource (Match) does not exist
+            // getMatch(Long id) is used here only to generate a link to a resource (methodOn creates a proxy)
+            // see here: https://docs.spring.io/spring-hateoas/docs/current/api/org/springframework/hateoas/mvc/ControllerLinkBuilder.html#methodOn-java.lang.Class-java.lang.Object...-
+
+            // this exception handling is needed only for syntactic purposes
+            // see here: https://github.com/spring-projects/spring-hateoas/issues/82
+
+            // also see: https://github.com/search?q=This+should+never+happen&type=Code&utf8=%E2%9C%93
+            throw new RuntimeException("getMatch threw an exception - this should never happen");
+        }
+
+        return matchResource;
     }
 }
