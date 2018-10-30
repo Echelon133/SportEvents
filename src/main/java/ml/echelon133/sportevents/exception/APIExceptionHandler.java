4a89d41cd0079ce3f7f68b7370695e1009498370
Mateusz Bednarski <echelon133@gmail.com>
2018-10-30 16:07:12 +0100

    Fix a conflict between match-package branch and master branch

--- src/main/java/ml/echelon133/sportevents/exception/APIExceptionHandler.java
+++ src/main/java/ml/echelon133/sportevents/exception/APIExceptionHandler.java
@@ -2,12 +2,15 @@ package ml.echelon133.sportevents.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import java.text.ParseException;
+
+import java.time.format.DateTimeParseException;
+
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
@@ -57,9 +60,11 @@ public class APIExceptionHandler extends ResponseEntityExceptionHandler {
         return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
     }
 
-    @ExceptionHandler(value = ParseException.class)
-    protected ResponseEntity<ErrorMessage> handleParseException(ParseException ex, WebRequest request) {
-        ErrorMessage message = new ErrorMessage(new Date(), request.getDescription(false), ex.getMessage());
+    @ExceptionHandler(value = DateTimeParseException.class)
+    protected ResponseEntity<ErrorMessage> handleDateTimeParseException(DateTimeParseException ex, WebRequest request) {
+        ErrorMessage message = new ErrorMessage(new Date(),
+                                                request.getDescription(false),
+                                  "Date could not be parsed");
         return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
     }
 }
