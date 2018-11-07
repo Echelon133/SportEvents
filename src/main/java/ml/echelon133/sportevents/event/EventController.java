1752855879578ab4ad0074aa717373f27926b686
Mateusz Bednarski <echelon133@gmail.com>
2018-11-07 12:48:01 +0100

    Remove all unnecessary imports from EventController

--- src/main/java/ml/echelon133/sportevents/event/EventController.java
+++ src/main/java/ml/echelon133/sportevents/event/EventController.java
@@ -1,21 +1,13 @@
 package ml.echelon133.sportevents.event;
 
 import ml.echelon133.sportevents.event.types.AbstractMatchEvent;
-import ml.echelon133.sportevents.event.types.GoalEvent;
-import ml.echelon133.sportevents.event.types.StandardEvent;
-import ml.echelon133.sportevents.event.types.SubstitutionEvent;
-import ml.echelon133.sportevents.event.types.dto.MatchEventDto;
 import ml.echelon133.sportevents.match.Match;
 import ml.echelon133.sportevents.match.MatchService;
-import ml.echelon133.sportevents.team.Team;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
 import java.util.List;
 
 @RestController
