0996bbc674e193f52550c2b90ccc932e8fac2f47
Mateusz Bednarski <echelon133@gmail.com>
2018-11-07 18:02:59 +0100

    Add an initial version of receiveEvent method to EventController. This implementation only validates events.

--- src/main/java/ml/echelon133/sportevents/event/EventController.java
+++ src/main/java/ml/echelon133/sportevents/event/EventController.java
@@ -1,13 +1,17 @@
 package ml.echelon133.sportevents.event;
 
 import ml.echelon133.sportevents.event.types.AbstractMatchEvent;
+import ml.echelon133.sportevents.event.types.dto.MatchEventDto;
+import ml.echelon133.sportevents.exception.FailedValidationException;
 import ml.echelon133.sportevents.match.Match;
 import ml.echelon133.sportevents.match.MatchService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -25,5 +29,14 @@ public class EventController {
     public ResponseEntity<List<AbstractMatchEvent>> getEvents(@PathVariable Long matchId) throws Exception {
         Match match = matchService.findById(matchId);
         return new ResponseEntity<>(match.getEvents(), HttpStatus.OK);
+    }
+
+    @PostMapping("/events")
+    public void receiveEvent(@PathVariable Long matchId, @Valid @RequestBody MatchEventDto eventDto, BindingResult result)
+            throws FailedValidationException{
+
+        if (result.hasErrors()) {
+            throw new FailedValidationException(result.getFieldErrors());
+        }
     }
 }
