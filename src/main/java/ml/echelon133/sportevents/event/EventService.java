aea1d2826c28ff73454f28bcd68309bf52789e4a
Mateusz Bednarski <echelon133@gmail.com>
2018-11-10 22:33:13 +0100

    Change processEvent method signature. 'Match' parameter is not needed, because objects extending AbstractMatchEvent have a reference to the Match instance

--- src/main/java/ml/echelon133/sportevents/event/EventService.java
+++ src/main/java/ml/echelon133/sportevents/event/EventService.java
@@ -1,8 +1,7 @@
 package ml.echelon133.sportevents.event;
 
 import ml.echelon133.sportevents.event.types.AbstractMatchEvent;
-import ml.echelon133.sportevents.match.Match;
 
 public interface EventService {
-    boolean processEvent(AbstractMatchEvent event, Match match) throws ProcessedEventRejectedException;
+    boolean processEvent(AbstractMatchEvent event) throws ProcessedEventRejectedException;
 }
