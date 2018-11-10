10db364d2a1827acd3e7d2e455b24938d86f2444
Mateusz Bednarski <echelon133@gmail.com>
2018-11-10 22:35:51 +0100

    Declare convertEventDtoToEntity method in EventService interface

--- src/main/java/ml/echelon133/sportevents/event/EventService.java
+++ src/main/java/ml/echelon133/sportevents/event/EventService.java
@@ -1,7 +1,11 @@
 package ml.echelon133.sportevents.event;
 
 import ml.echelon133.sportevents.event.types.AbstractMatchEvent;
+import ml.echelon133.sportevents.event.types.dto.MatchEventDto;
+import ml.echelon133.sportevents.match.Match;
 
 public interface EventService {
     boolean processEvent(AbstractMatchEvent event) throws ProcessedEventRejectedException;
+    AbstractMatchEvent convertEventDtoToEntity(MatchEventDto eventDto, Match eventMatch)
+            throws ProcessedEventRejectedException;
 }
