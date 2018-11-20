f4e229d37da4785c0e39699fd36d60a6f389b227
Mateusz Bednarski <echelon133@gmail.com>
2018-11-20 14:06:17 +0100

    Update TestUtils with static methods that can generate Match instances with random data

--- src/test/java/ml/echelon133/sportevents/TestUtils.java
+++ src/test/java/ml/echelon133/sportevents/TestUtils.java
@@ -12,7 +12,9 @@ import ml.echelon133.sportevents.team.Team;
 import ml.echelon133.sportevents.team.TeamResource;
 import ml.echelon133.sportevents.team.TeamResourceAssembler;
 
+import java.nio.charset.Charset;
 import java.util.Date;
+import java.util.Random;
 
 
 public class TestUtils {
@@ -21,12 +23,14 @@ public class TestUtils {
     private static TeamResourceAssembler teamResourceAssembler;
     private static MatchResourceAssembler matchResourceAssembler;
     private static StadiumResourceAssembler stadiumResourceAssembler;
+    private static Random random;
 
     static {
         leagueResourceAssembler = new LeagueResourceAssembler();
         stadiumResourceAssembler = new StadiumResourceAssembler();
         teamResourceAssembler = new TeamResourceAssembler(leagueResourceAssembler);
         matchResourceAssembler = new MatchResourceAssembler(teamResourceAssembler, leagueResourceAssembler, stadiumResourceAssembler);
+        random = new Random();
     }
 
     public static Stadium buildStadium(Long id, String stadiumName, String stadiumCity, Integer stadiumCapacity) {
@@ -51,6 +55,42 @@ public class TestUtils {
         Match testMatch = new Match(new Date(), teamA, teamB, league, stadium);
         testMatch.setId(id);
         return testMatch;
+    }
+
+    public static Match getRandomMatch() {
+        long matchId = random.nextLong();
+        long leagueId = random.nextLong();
+        long teamAId = random.nextLong();
+        long teamBId = random.nextLong();
+        String leagueName = getRandomString();
+        String teamAName = getRandomString();
+        String teamBName = getRandomString();
+
+        League league = buildLeague(leagueId, leagueName, "Country");
+        Team teamA = buildTeam(teamAId, teamAName, league);
+        Team teamB = buildTeam(teamBId, teamBName, league);
+        return buildMatch(matchId, teamA, teamB, league, null);
+    }
+
+    public static Match getRandomMatch(Long matchId) {
+        long leagueId = random.nextLong();
+        long teamAId = random.nextLong();
+        long teamBId = random.nextLong();
+        String leagueName = getRandomString();
+        String teamAName = getRandomString();
+        String teamBName = getRandomString();
+
+        League league = buildLeague(leagueId, leagueName, "Country");
+        Team teamA = buildTeam(teamAId, teamAName, league);
+        Team teamB = buildTeam(teamBId, teamBName, league);
+        return buildMatch(matchId, teamA, teamB, league, null);
+    }
+
+    private static String getRandomString() {
+        int length = random.nextInt(20);
+        byte[] bytes = new byte[length];
+        random.nextBytes(bytes);
+        return new String(bytes, Charset.defaultCharset());
     }
 
     public static TeamResource buildTeamResource(Team team) {
