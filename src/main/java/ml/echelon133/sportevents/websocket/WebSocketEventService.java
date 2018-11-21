cc14cdb61d366e15191bcbe373bb91603f71dd73
Mateusz Bednarski <echelon133@gmail.com>
2018-11-21 19:05:34 +0100

    Make WebSocketEventService's only method accept String with fully formed websocketPath, instead of taking a Long and creating the path on the spot

--- src/main/java/ml/echelon133/sportevents/websocket/WebSocketEventService.java
+++ src/main/java/ml/echelon133/sportevents/websocket/WebSocketEventService.java
@@ -3,5 +3,5 @@ package ml.echelon133.sportevents.websocket;
 import ml.echelon133.sportevents.event.types.AbstractMatchEvent;
 
 public interface WebSocketEventService {
-    void sendEventOverWebSocket(Long matchId, AbstractMatchEvent event);
+    void sendEventOverWebSocket(String websocketPath, AbstractMatchEvent event);
 }
